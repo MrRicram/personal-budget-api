f92cf75819ff1098594a05ea13bba1fdfb46bb96
Ricardo Ramos <rf.ramos95@gmail.com>
2021-04-24 15:21:46 +0100

    Implement api route

---
+++ server/api.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const envelopesRouter = require('./envelopes');
+
+router.use('/envelopes', envelopesRouter);
+
+module.exports = router;
